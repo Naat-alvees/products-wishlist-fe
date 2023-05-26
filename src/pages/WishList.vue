5764ce51e1a5d201dc81f8ab302c419066d7083a
Natasha Castro <nathy-ac@hotmail.com>
2023-05-26 02:28:20 -0300

    Loading e mensagem de erro na wishlist

--- src/pages/WishList.vue
+++ src/pages/WishList.vue
@@ -1,17 +1,26 @@
 <template>
+  <Loading v-if="loadingWishList" />
+  <Error v-if="errorWishList" />
+  <p v-if="!wishList.length">Sua lista de desejos está vazia :(</p>
   <ListProduct :listProduct="wishList" :isWishList="true" />
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
+import Error from '../components/Error.vue'
 import ListProduct from '../containers/ListProduct.vue'
+import Loading from '../components/Loading.vue'
 
 export default {
   name: 'WishList',
   components: {
-    ListProduct
+    Error,
+    ListProduct,
+    Loading
   },
-  computed: mapState(['wishList'])
+  computed: {
+    ...mapState(['loadingWishList', 'errorWishList', 'wishList'])
+  }
 }
 </script>
