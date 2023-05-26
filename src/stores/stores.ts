8a86a51c35b0388740fa99e36ba441c17c4b00a9
Natasha Castro <nathy-ac@hotmail.com>
2023-05-26 02:18:18 -0300

    Operações wishLlist na API

--- src/stores/stores.ts
+++ src/stores/stores.ts
@@ -6,16 +6,21 @@ interface State {
   loadingProducts: boolean
   errorProducts: boolean
   loadingWishList: boolean
+  errorWishList: boolean
   products: ShortProduct[]
   wishList: ShortProduct[]
   searchFilter: string
 }
+
+const URL_PRODUCTS = 'http://localhost:3000/products'
+const URL_WISHLIST = 'http://localhost:3000/wishList'
 
 export default createStore<State>({
   state: {
     loadingProducts: false,
     errorProducts: false,
     loadingWishList: false,
+    errorWishList: false,
     products: [],
     wishList: [],
     searchFilter: ''
@@ -41,25 +46,48 @@ export default createStore<State>({
     setProducts(state, items: ShortProduct[]) {
       state.products = items
     },
-    addWishList(state, produc: ShortProduct) {
-      state.wishList.push(produc)
+    addWishList(state, product: ShortProduct) {
+      axios
+        .post(URL_WISHLIST, product)
+        .then(() => {
+          state.wishList.push(product)
+        })
+        .catch(() => {
+          state.errorProducts = true
+        })
     },
     removeWishList(state, idProduct: number) {
-      state.wishList = state.wishList.filter(product => product.id !== idProduct)
+      axios
+        .delete(`${URL_WISHLIST}/${idProduct}`)
+        .then(() => {
+          state.wishList = state.wishList.filter(product => product.id !== idProduct)
+        })
+        .catch(() => {
+          state.errorProducts = true
+        })
     }
   },
   actions: {
     async loadItems({ commit }: any) {
       this.state.loadingProducts = true
+      this.state.loadingWishList = true
 
-      try {
-        const response = await axios.get('http://localhost:3000/products')
-        commit('setProducts', response.data.products)
-      } catch (error) {
-        this.state.errorProducts = true
-      } finally {
-        this.state.loadingProducts = false
-      }
+      axios
+        .all([axios.get(URL_PRODUCTS), axios.get(URL_WISHLIST)])
+        .then(
+          axios.spread((responseProduct, responseWishList) => {
+            this.state.products = responseProduct.data.products
+            this.state.wishList = responseWishList.data.products
+          })
+        )
+        .catch(() => {
+          this.state.errorProducts = true
+          this.state.errorWishList = true
+        })
+        .finally(() => {
+          this.state.loadingProducts = false
+          this.state.loadingWishList = false
+        })
     }
   }
 })
